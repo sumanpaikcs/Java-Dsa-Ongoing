4732df74270af1b690e75ec882924e0558484453
Suman <sumanpaikcs@gmail.com>
2021-11-04 16:29:07 +0530

    This is the first lesson of my java dsa bootcamp

---
+++ f1_javapgrm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class f1_javapgrm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        int a = sc.nextInt();
+        System.out.println("The number you entered is :"+ a);
+
+        System.out.println("Enter the second number :");
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println("The number you entered is :"+ b);
+
+        
+    }
+}
