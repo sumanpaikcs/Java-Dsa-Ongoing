5cd5c04fd913db10f5093c42eb94201710561240
Suman <sumanpaikcs@gmail.com>
2021-11-11 12:39:38 +0530

    Store something in ajva array string

---
+++ storesomething.java
@@ -0,0 +1,28 @@
+public class storesomething {
+    public static void main(String[] args) {
+        int a = 20 ;
+        System.out.println(a);
+
+        String name = "java cource";
+        System.out.println(name);
+
+        String[] nmm = {"java", "cpp", "pyhton"};    // String array
+        System.out.println(nmm[2]);
+        //syntax
+        //Datatype[] var_name = new datatype[size];
+
+     //   int [] roll = new int[6];
+            // or
+       // int [] roll2 = {1,2,3,4,5,6};
+
+
+
+        String[] arr = new String[4];
+        System.out.println(arr[1]);
+
+
+        
+
+    }
+    
+}
