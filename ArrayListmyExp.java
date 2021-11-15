887d201a8b02acfba0d6d996c8aadc009a197070
Suman <sumanpaikcs@gmail.com>
2021-11-15 21:25:03 +0530

    arraylist going to be covered

---
+++ ArrayListmyExp.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ArrayListmyExp {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+    
+}
