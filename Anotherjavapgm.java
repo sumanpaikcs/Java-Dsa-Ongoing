5f889f18eb6658c0e9fbd05540d92d22b3c1f56b
Suman Paik <sumanpaikcs@gmail.com>
2021-11-30 22:05:51 +0530

    rename another file

--- Anotherjavapgm.java
+++ Anotherjavapgm.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class f2_javapgm{
+public class Anotherjavapgm{
     public static void main(String[] args ){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
