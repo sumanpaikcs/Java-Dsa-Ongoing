a1f853790a5e1bf4d58268a7e3e64ce70ab8af3f
Suman Paik <sumanpaikcs@gmail.com>
2021-11-30 22:03:36 +0530

    rename file

--- Myjavapgrm.java
+++ Myjavapgrm.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class f1_javapgrm {
+public class Myjavapgrm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number :");
